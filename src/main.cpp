88214652b808a8900c590d9267d0f7f0e2263dd9
Sveppg <terminator12327@gmail.com>
2024-07-02 15:16:34 +0200

    design changes

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,16 @@ int main(){
     
     Color pink = {255, 97, 202, 255};
     Color grey = {29, 29, 27, 255};
-    Color yellow = {243, 216,63, 255};
+    Color magenta = {133, 133, 173, 255};
+
+    //Color yellow = {243, 216,63, 255};
     int offset = 50;
     int windowWidth = 750; 
     int windowHeight = 700;
        
     InitWindow(windowWidth +offset, windowHeight + 2*offset, "C++ Space Invaders");
     
-    Font font = LoadFontEx("Font/JABROWZKY.ttf",64, 0, 0);
+    Font font = LoadFontEx("Font/GrahamBlack-PV3xg.ttf",64, 0, 0);
     Texture2D spaceshipimage = LoadTexture("Graphics/spaceship.png");
     SetTargetFPS(60);
 
@@ -30,25 +32,25 @@ int main(){
         game.Update();
         BeginDrawing();
         ClearBackground(grey);
-        DrawRectangleRoundedLines({10,10,780,780,},0.18f,20,2,yellow);
-        DrawLineEx({25, 730},{775, 730}, 3, pink);
+        //DrawRectangleRoundedLines({10,10,780,780,},0.18f,20,2,yellow);
+        DrawLineEx({25, 730},{775, 730}, 3, magenta);
         if(game.run){
-            DrawTextEx(font, "LEVEL 01", {540, 740}, 30, 2, pink);    
+            DrawTextEx(font, "LEVEL 01", {540, 740}, 30, 2, magenta);    
         }else{
-            DrawTextEx(font, "GAME OVER!", {540, 740}, 30, 2, pink);
+            DrawTextEx(font, "GAME OVER!", {540, 740}, 30, 2, magenta);
         }
         float x = 50.0;
         for(int i = 0; i  < game.lives; i++){
             DrawTextureV(spaceshipimage, {x, 745}, WHITE);
             x += 50.0;
         }
-        DrawTextEx(font,"SCORE", {50, 15},30, 2, pink);
+        DrawTextEx(font,"SCORE", {50, 15},30, 2, magenta);
         std::string scoreText = FormatWithLeadingZeros(game.score, 5);
-        DrawTextEx(font, scoreText.c_str(), {50, 40}, 30, 2, pink);
+        DrawTextEx(font, scoreText.c_str(), {50, 40}, 30, 2, magenta);
         
         DrawTextEx(font, "HIGH SCORE", {570, 15}, 34, 2, pink);
         std::string highscoreText = FormatWithLeadingZeros(game.highscore, 5);
-        DrawTextEx(font, highscoreText.c_str(), {655, 40}, 43, 2, pink);
+        DrawTextEx(font, highscoreText.c_str(), {655, 40}, 43, 2, magenta);
         
         game.Draw();
         EndDrawing();
